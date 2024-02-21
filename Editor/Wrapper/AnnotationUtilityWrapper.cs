bb784109d77c4698e46e4da7455c13bd56c6ad53
Markus <marco.polo.1994@web.de>
2024-02-21 13:09:03 +0100

    AnnotationUtilityWrapper now supports classname

--- Editor/Wrapper/AnnotationUtilityWrapper.cs
+++ Editor/Wrapper/AnnotationUtilityWrapper.cs
@@ -75,5 +75,61 @@ namespace EditorHelper {
             gizmoEnabled = false;
             return false;
         }
+
+        /// <summary>
+        /// ToggleGizmo by ClassID -> https://docs.unity3d.com/Manual/ClassIDReference.html
+        /// </summary>
+        /// <param name="enabled"></param>
+        /// <param name="className"></param>
+        public static void ToggleGizmo(bool enabled, string className) {
+            int val = enabled ? 1 : 0;
+            if (type != null) {
+                object annotations = getAnnotations.Invoke(null, null);
+                foreach (object annotation in (IEnumerable)annotations) {
+                    Type annotationType = annotation.GetType();
+                    FieldInfo classIdField = annotationType.GetField("classID", BindingFlags.Public | BindingFlags.Instance);
+                    FieldInfo scriptClassField = annotationType.GetField("scriptClass", BindingFlags.Public | BindingFlags.Instance);
+                    if (classIdField != null && scriptClassField != null) {
+                        int id = (int)classIdField.GetValue(annotation);
+                        string scriptClass = (string)scriptClassField.GetValue(annotation);
+                        if (className == scriptClass) {
+                            setGizmoEnabled.Invoke(null, new object[] { id, scriptClass, val, false });
+                            setIconEnabled.Invoke(null, new object[] { id, scriptClass, val });
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Try to get the state of the Gizmo with the given ClassID -> https://docs.unity3d.com/Manual/ClassIDReference.html
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="gizmoEnabled"></param>
+        /// <param name="iconEnabled"></param>
+        /// <returns></returns>
+        public static bool TryGetGizmoToggleState(string className, out bool gizmoEnabled, out bool iconEnabled) {
+            if (type != null) {
+                object annotations = getAnnotations.Invoke(null, null);
+                foreach (object annotation in (IEnumerable)annotations) {
+                    Type annotationType = annotation.GetType();
+                    FieldInfo scriptClassField = annotationType.GetField("scriptClass", BindingFlags.Public | BindingFlags.Instance);
+                    FieldInfo iconEnabledField = annotationType.GetField("iconEnabled", BindingFlags.Public | BindingFlags.Instance);
+                    FieldInfo gizmoEnabledField = annotationType.GetField("gizmoEnabled", BindingFlags.Public | BindingFlags.Instance);
+                    if (scriptClassField != null) {
+                        string scriptClass = (string)scriptClassField.GetValue(annotation);
+                        if (className == scriptClass) {
+                            iconEnabled = ((int)iconEnabledField.GetValue(annotation)) != 0;
+                            gizmoEnabled = ((int)gizmoEnabledField.GetValue(annotation)) != 0;
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            iconEnabled = false;
+            gizmoEnabled = false;
+            return false;
+        }
     }
 }
