8923dd4eee3cf11a95a0e0010d453f005441dc95
Markus <marco.polo.1994@web.de>
2024-03-07 13:37:13 +0100

    fix an {} issue

--- Editor/MeshDrawer.cs
+++ Editor/MeshDrawer.cs
@@ -44,104 +44,103 @@ namespace EditorHelper {
                 }
             }
         }
+    }
 
+    public class MeshVisual {
+        public List<MeshRenderBundle> meshRenderBundle;
+        GameObject asset;
+        public MaterialPropertyBlock globalMatPropertyBlock;
+        Func<GameObject, MaterialPropertyBlock> matPropertyBlockExtration;
+        bool includeChildren;
 
-        public class MeshVisual {
-            public List<MeshRenderBundle> meshRenderBundle;
-            GameObject asset;
-            public MaterialPropertyBlock globalMatPropertyBlock;
-            Func<GameObject, MaterialPropertyBlock> matPropertyBlockExtration;
-            bool includeChildren;
+        public MeshVisual(GameObject asset, MaterialPropertyBlock materialPropertyBlock = null, Func<GameObject, MaterialPropertyBlock> matPropertyBlockExtration = null, bool includeChildren = true) {
+            this.asset = asset;
+            globalMatPropertyBlock = materialPropertyBlock;
+            this.matPropertyBlockExtration = matPropertyBlockExtration;
+            this.includeChildren = includeChildren;
 
-            public MeshVisual(GameObject asset, MaterialPropertyBlock materialPropertyBlock = null, Func<GameObject, MaterialPropertyBlock> matPropertyBlockExtration = null, bool includeChildren = true) {
-                this.asset = asset;
-                globalMatPropertyBlock = materialPropertyBlock;
-                this.matPropertyBlockExtration = matPropertyBlockExtration;
-                this.includeChildren = includeChildren;
+            meshRenderBundle = new List<MeshRenderBundle>();
 
-                meshRenderBundle = new List<MeshRenderBundle>();
+            Update();
+        }
 
-                Update();
+        public void Update() {
+            meshRenderBundle.Clear();
+
+            MeshRenderer[] meshRenderer = new MeshRenderer[1];
+
+            if (includeChildren) {
+                meshRenderer = asset.GetComponentsInChildren<MeshRenderer>();
+            } else {
+                asset.TryGetComponent(out meshRenderer[0]);
             }
-
-            public void Update() {
-                meshRenderBundle.Clear();
-
-                MeshRenderer[] meshRenderer = new MeshRenderer[1];
-
-                if (includeChildren) {
-                    meshRenderer = asset.GetComponentsInChildren<MeshRenderer>();
-                } else {
-                    asset.TryGetComponent(out meshRenderer[0]);
+            for (int i = 0; i < meshRenderer.Length; i++) {
+                if (meshRenderer[i] == null) {
+                    continue;
                 }
-                for (int i = 0; i < meshRenderer.Length; i++) {
-                    if (meshRenderer[i] == null) {
-                        continue;
-                    }
-                    Mesh mesh = meshRenderer[i].GetComponent<MeshFilter>().sharedMesh;
-                    Material[] materials = meshRenderer[i].sharedMaterials;
+                Mesh mesh = meshRenderer[i].GetComponent<MeshFilter>().sharedMesh;
+                Material[] materials = meshRenderer[i].sharedMaterials;
 
-                    if (matPropertyBlockExtration != null) {
-                        globalMatPropertyBlock = matPropertyBlockExtration.Invoke(meshRenderer[i].gameObject);
-                    }
-
-                    for (int j = 0; j < materials.Length; j++) {
-                        meshRenderBundle.Add(new MeshRenderBundle(mesh, materials[j], globalMatPropertyBlock, j, meshRenderer[i].transform.position, meshRenderer[i].transform.rotation, meshRenderer[i].transform.lossyScale));
-                    }
+                if (matPropertyBlockExtration != null) {
+                    globalMatPropertyBlock = matPropertyBlockExtration.Invoke(meshRenderer[i].gameObject);
                 }
 
-                SkinnedMeshRenderer[] skinnedMeshRenderer = new SkinnedMeshRenderer[1];
-                if (includeChildren) {
-                    skinnedMeshRenderer = asset.GetComponentsInChildren<SkinnedMeshRenderer>();
-                } else {
-                    asset.TryGetComponent(out skinnedMeshRenderer[0]);
-                }
-                for (int i = 0; i < skinnedMeshRenderer.Length; i++) {
-                    if (skinnedMeshRenderer[i] == null) {
-                        continue;
-                    }
-                    Mesh mesh = skinnedMeshRenderer[i].sharedMesh;
-                    Material[] materials = skinnedMeshRenderer[i].sharedMaterials;
-
-                    if (matPropertyBlockExtration != null) {
-                        globalMatPropertyBlock = matPropertyBlockExtration.Invoke(skinnedMeshRenderer[i].gameObject);
-                    }
-
-                    for (int j = 0; j < materials.Length; j++) {
-                        meshRenderBundle.Add(new MeshRenderBundle(mesh, materials[j], globalMatPropertyBlock, j, skinnedMeshRenderer[i].transform.position, skinnedMeshRenderer[i].transform.rotation, skinnedMeshRenderer[i].transform.lossyScale));
-                    }
+                for (int j = 0; j < materials.Length; j++) {
+                    meshRenderBundle.Add(new MeshRenderBundle(mesh, materials[j], globalMatPropertyBlock, j, meshRenderer[i].transform.position, meshRenderer[i].transform.rotation, meshRenderer[i].transform.lossyScale));
                 }
             }
 
-            public void ToggleKeyword(bool enable, string keyword) {
-                for (int i = 0; i < meshRenderBundle.Count; i++) {
-                    if (enable) {
-                        meshRenderBundle[i].material.EnableKeyword(keyword);
-                    } else {
-                        meshRenderBundle[i].material.DisableKeyword(keyword);
-                    }
+            SkinnedMeshRenderer[] skinnedMeshRenderer = new SkinnedMeshRenderer[1];
+            if (includeChildren) {
+                skinnedMeshRenderer = asset.GetComponentsInChildren<SkinnedMeshRenderer>();
+            } else {
+                asset.TryGetComponent(out skinnedMeshRenderer[0]);
+            }
+            for (int i = 0; i < skinnedMeshRenderer.Length; i++) {
+                if (skinnedMeshRenderer[i] == null) {
+                    continue;
+                }
+                Mesh mesh = skinnedMeshRenderer[i].sharedMesh;
+                Material[] materials = skinnedMeshRenderer[i].sharedMaterials;
+
+                if (matPropertyBlockExtration != null) {
+                    globalMatPropertyBlock = matPropertyBlockExtration.Invoke(skinnedMeshRenderer[i].gameObject);
+                }
+
+                for (int j = 0; j < materials.Length; j++) {
+                    meshRenderBundle.Add(new MeshRenderBundle(mesh, materials[j], globalMatPropertyBlock, j, skinnedMeshRenderer[i].transform.position, skinnedMeshRenderer[i].transform.rotation, skinnedMeshRenderer[i].transform.lossyScale));
                 }
             }
         }
 
-        public class MeshRenderBundle {
-            public Mesh mesh;
-            public Material material;
-            public MaterialPropertyBlock materialPropertyBlock;
-            public int submeshIndex;
-            public Vector3 positionOffset;
-            public Quaternion rotationOffset;
-            public Vector3 scaleOffset;
-
-            public MeshRenderBundle(Mesh mesh, Material material, MaterialPropertyBlock materialPropertyBlock, int submeshIndex, Vector3 positionOffset, Quaternion rotationOffset, Vector3 scaleOffset) {
-                this.mesh = mesh;
-                this.material = material;
-                this.materialPropertyBlock = materialPropertyBlock;
-                this.submeshIndex = submeshIndex;
-                this.positionOffset = positionOffset;
-                this.rotationOffset = rotationOffset;
-                this.scaleOffset = scaleOffset;
+        public void ToggleKeyword(bool enable, string keyword) {
+            for (int i = 0; i < meshRenderBundle.Count; i++) {
+                if (enable) {
+                    meshRenderBundle[i].material.EnableKeyword(keyword);
+                } else {
+                    meshRenderBundle[i].material.DisableKeyword(keyword);
+                }
             }
+        }
+    }
+
+    public class MeshRenderBundle {
+        public Mesh mesh;
+        public Material material;
+        public MaterialPropertyBlock materialPropertyBlock;
+        public int submeshIndex;
+        public Vector3 positionOffset;
+        public Quaternion rotationOffset;
+        public Vector3 scaleOffset;
+
+        public MeshRenderBundle(Mesh mesh, Material material, MaterialPropertyBlock materialPropertyBlock, int submeshIndex, Vector3 positionOffset, Quaternion rotationOffset, Vector3 scaleOffset) {
+            this.mesh = mesh;
+            this.material = material;
+            this.materialPropertyBlock = materialPropertyBlock;
+            this.submeshIndex = submeshIndex;
+            this.positionOffset = positionOffset;
+            this.rotationOffset = rotationOffset;
+            this.scaleOffset = scaleOffset;
         }
     }
 }
